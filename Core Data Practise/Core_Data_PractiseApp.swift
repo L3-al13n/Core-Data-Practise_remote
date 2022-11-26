3bb693657e58a3562506611685acef702c3ea7f3
The Alien <tbadwan2022@icloud.com>
2022-11-26 15:50:12 +0100

    Initial Commit

---
+++ Core Data Practise/Core_Data_PractiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Core_Data_PractiseApp.swift
+//  Core Data Practise
+//
+//  Created by Tariq Badwan on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Core_Data_PractiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

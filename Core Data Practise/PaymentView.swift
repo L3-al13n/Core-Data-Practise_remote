99270234fc4a5d8624fcda5fa8a012ecb033f0cd
The Alien <tbadwan2022@icloud.com>
2022-11-26 16:06:43 +0100

    Create View for second gateway

---
+++ Core Data Practise/PaymentView.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentView.swift
+//  Core Data Practise
+//
+//  Created by Tariq Badwan on 26/11/2022.
+//
+
+import SwiftUI
+
+struct PaymentView: View {
+    var body: some View {
+        Text("I am practising Branching")
+    }
+}
+
+struct PaymentView_Previews: PreviewProvider {
+    static var previews: some View {
+        PaymentView()
+    }
+}
